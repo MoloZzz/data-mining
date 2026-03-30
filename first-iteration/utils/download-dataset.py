c79137cb4b3a0901606319b7376008818d56f3e1
MoloZzz <mega_oleksii@ukr.net>
2026-03-30 03:46:03 +0300

    chore: download datasets

--- first-iteration/utils/download-dataset.py
+++ first-iteration/utils/download-dataset.py
@@ -2,7 +2,9 @@ import kagglehub
 
 dataset1 = "rouseguy/bankbalanced"
 dataset2 = "prasad22/retail-transactions-dataset"
+dataset3 = "crawlfeeds/tesco-uk-groceries-dataset"
+dataset4 = "vjchoudhary7/customer-segmentation-tutorial-in-python"
 
-path = kagglehub.dataset_download(dataset2)
+path = kagglehub.dataset_download(dataset4)
 
 print("Path to dataset files:", path)
